06053b6330a97e957e07f848c63d21c38afc8e5c
shimpali <shimpali.deshpande@gmail.com>
2023-07-22 18:45:49 +0530

    shortener_app

--- shortener_app/config.py
+++ shortener_app/config.py
@@ -1,7 +1,8 @@
+# shortener_app/config.py
+
 from functools import lru_cache
 
 from pydantic import BaseSettings
-
 
 class Settings(BaseSettings):
     env_name: str = "Local"
@@ -11,10 +12,8 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
-
 @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
     return settings
-
